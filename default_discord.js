71ea2f71aef549872fb0c5a7a90ca3be2479c38c
Max <hopsmax101@gmail.com>
2021-09-15 16:13:00 -0700

    beginning of discord bot

---
+++ default_discord.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const YAML = require('js-yaml');
+const fs = require('fs');
+
+//const auth = require('./token.json');
+const client = new Discord.Client();
+
+
+var prefix = "z!";
+const token = fs.readFileSync('./token.yaml', 'utf8');
+
+//Important (initializes bot)
+client.once('ready', () => {
+	console.log('Ready!');
+
+});
+
+
+const regCMD = new RegExp(prefix+"(.*)");
+
+client.on('message', msg => {
+if(regCMD.test(msg)){
+	const match = msg.content.match(regCMD);//array of regex match
+	switch(match[1]){
+		case "hi":
+			const user = msg.author.id;
+			console.log(user);
+			msg.channel.send("hello <@" + user + ">!");
+		break;
+	}
+
+	
+}
+else{
+    console.log(msg.author.username+": "+msg);
+}
+});
+
+
+//Important (logs in)
+//client.login(token.token); //for json
+client.login(YAML.safeLoad(token).token);
